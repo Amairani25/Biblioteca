ba719a36d1bf2a810d1c40f5f67b0df847e06b5c
amair <amair@LAPTOP-Q9P3M1OQ>
2017-11-10 22:43:15 -0600

    Add project files.

---
+++ Biblioteca/Bibliotecaa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Bibliotecaa
+    {
+        List<Libro> l = new List<Libro>();
+
+        public int cont = 0;
+        public string Nombre { get; set; }
+
+
+        public Bibliotecaa(string Nombre)
+        {
+            this.Nombre = Nombre;
+
+        }
+        public void Agregar(string nomb, string edit, string aut)
+        {
+            Libro l1 = new Libro(nomb, edit, aut);
+            l.Add(l1);
+            cont++;
+
+        }
+        public void Consultar(string a)
+        {
+            bool f = true;
+            for (int i = 0; i < l.Count; i++)
+            {
+                if (l[i].Nombre == a)
+                {
+                    Console.WriteLine("Se encontro " + l[i].Nombre);
+                }
+                if (l[i].Nombre != a)
+                {
+                    if (f == true)
+                    {
+                        Console.WriteLine("No se encontro " + l[i].Nombre);
+                        f = false;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Biblioteca/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Program
+    {
+        static byte a;
+        static void Main(string[] args)
+        {
+
+            string c = "";
+            string nom = "";
+            string edit = "";
+            string aut = "";
+            Console.WriteLine("Ingresa el nombre de la primer biblioteca");
+            c = Console.ReadLine();
+            Bibliotecaa b1 = new Bibliotecaa(c);
+            Console.WriteLine("Ingrese el nombre de la segunda biblioteca");
+            c = Console.ReadLine();
+            Bibliotecaa b2 = new Bibliotecaa(c);
+            Console.WriteLine("Ingrese el nombre de la tercera biblioteca");
+            c = Console.ReadLine();
+            Bibliotecaa b3 = new Bibliotecaa(c);
+
+            do
+            {
+                Menu();
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("En cual biblioteca quieres agregar");
+                        c = Console.ReadLine();
+                        if (c == b1.Nombre)
+                        {
+                            Console.WriteLine("Ingrese el titulo del libro");
+                            nom = Console.ReadLine();
+                            Console.WriteLine("Ingrese la editorial");
+                            edit = Console.ReadLine();
+                            Console.WriteLine("Ingrese el autor");
+                            aut = Console.ReadLine();
+
+                            b1.Agregar(nom, edit, aut);
+                        }
+                        if (c == b2.Nombre)
+                        {
+                            Console.WriteLine("Ingrese el titulo del libro");
+                            nom = Console.ReadLine();
+                            Console.WriteLine("Ingrese la editorial");
+                            edit = Console.ReadLine();
+                            Console.WriteLine("Ingrese el autor");
+                            aut = Console.ReadLine();
+
+                            b2.Agregar(nom, edit, aut);
+                        }
+                        if (c == b3.Nombre)
+                        {
+                            Console.WriteLine("Ingrese el titulo del libro");
+                            nom = Console.ReadLine();
+                            Console.WriteLine("Ingrese la editorial");
+                            edit = Console.ReadLine();
+                            Console.WriteLine("Ingrese el autor");
+                            aut = Console.ReadLine();
+
+                            b3.Agregar(nom, edit, aut);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("¿Cual biblioteca quieres consultar?");
+                        c = Console.ReadLine();
+                        if (c == b1.Nombre)
+                        {
+                            Console.WriteLine("Ingresa el titulo del libro");
+                            nom = Console.ReadLine();
+                            b1.Consultar(nom);
+                        }
+                        if (c == b2.Nombre)
+                        {
+                            Console.WriteLine("Ingresa el titulo del libro");
+                            nom = Console.ReadLine();
+                            b2.Consultar(nom);
+                        }
+                        if (c == b3.Nombre)
+                        {
+                            Console.WriteLine("Ingresa el  titulo del libro");
+                            nom = Console.ReadLine();
+                            b2.Consultar(nom);
+                        }
+                        break;
+
+
+                }
+
+            }
+            while (a != 0);
+
+        }
+
+        static void Menu()
+        {
+
+            Console.WriteLine("Elija una opcion:");
+            Console.WriteLine(" 1-Agregar \n 2-Consultar");
+            a = byte.Parse(Console.ReadLine());
+
+        }
+    }
+}
